f4fc2821e5678606b84443244afa4ac623867e4e
Jonathan Champion <champij5@ferris.edu>
2015-11-12 23:18:57 -0500

    PHP, because reasons
    Because of the PHP, you will need to have a local server in order to
    view pages correctly (including when working on the Game portion)

--- play.php
+++ play.php
@@ -1,8 +1,12 @@
 <!--
 /*******************************************/
-/*---------GORDON YASMAR ABOUT PAGE--------*/
+/*---------GORDON YASMAR PLAY PAGE---------*/
 /*-----------------------------------------*/
 /*******************************************/
+
+/********************************/
+/*------------Summary-----------*/
+/********************************/
 
 /********************************/
 /*--------------TEAM------------*/
@@ -12,19 +16,26 @@ Jonathan Champion
 Eric Selover
 
 
-/-->
 
+/-->
 <!doctype html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>About - Gordon Yasmar</title>
+    <title>Play - Gordon Yasmar</title>
     <link href="style.css" rel="stylesheet">
     <link href="grid.css" rel="stylesheet">
 </head>
+    <style>
+    </style>
 <body>
+    <? include 'header.php' ?>
 
+    <canvas width="1280" height="720" id="canvas"> </canvas>
 
-
+    <script src="game.js">
+      
+    </script>
+    
 </body>
 </html>
